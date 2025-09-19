e755b0204e1c43b550ff4191e6b3a32720c69e44
SmellyFella <matthewjdorosz@outlook.com>
2025-09-19 11:30:28 +0800

    Smart grid updated Config.py

--- Config.py
+++ Config.py
@@ -3,32 +3,32 @@ class Config(object):
     # System setup
     N_METER             = 20                   # Number of Smart meters
     N_SUBSTATION           = 2                    # Number of Fog Servers
-    METER_COMP_CAP      = 3                 # Max reporting rate (kW) for meters
-    METER_TRAN_CAP      = 2                   # Max communication bandwidth (kW)
-    SUBSTATION_COMP_CAP    = 100                   # Substation Servers Handling Capacity (kWh)
+    METER_GEN_CAP_KW      = 5                 # Max reporting rate (kW) for meters
+    METER_LINE_CAP_KW      = 20                   # Max communication bandwidth (kW)
+    SUBSTATION_TRANS_CAP_KW    = 500                   # Substation Servers Handling Capacity (kWh)
 
     # Energy consumption settings
     METER_ENERGY_STATE  = [0.25, 0.50, 0.75]   # Meter Power profiles (idle, normal, peak) in kWh
-    METER_COMP_ENERGY   = 0.01                    # Energy cost of sampling in kWh
-    METER_TRAN_ENERGY   = 0.005                  # Cost of energy to transmit reading to a substation
+    METER_PROC_LOSS   = 0.01                    # Energy cost of sampling in kWh
+    METER_LINE_LOSS   = 0.03                  # Cost of energy to transmit reading to a substation
     METER_IDLE_ENERGY   = 0.001                  # Energy draw whilst idle
     SUBSTATION_COMP_ENERGY = 0.5                    # Energy to process readings or route power flows
 
     # Task Requirement
-    TASK_COMP_DENS   = [1, 3, 5]      # Task Energy Density
+    TASK_URGENCY_FACTOR  = [1, 3, 5]      # Task Energy Density
 
-    TASK_MIN_SIZE    = 1
-    TASK_MAX_SIZE    = 7
+    TASK_MIN_SIZE    = 0.5
+    TASK_MAX_SIZE    = 20
     N_COMPONENT      = 1                    # Number of Task Partitions
     MAX_DELAY        = 10
 
     # Deadlines (in timesteps) for different task types
-    CRITICAL_DEADLINE = 2      # must be processed within 2 timesteps
-    NONCRITICAL_DEADLINE = 6   # non-critical tasks can wait longer
+    CRITICAL_DEADLINE = 1      # must be processed within 2 timesteps
+    NONCRITICAL_DEADLINE = 5   # non-critical tasks can wait longer
     
     # Latency introduced by communication/processing
-    FIXED_COMM_DELAY = 1       # time to send reading to substation from meter
-    PROC_DELAY_FACTOR = 0.5    # queueing or control latency at substations/fog servers
+    FIXED_COMM_DELAY = 0.2       # time to send reading to substation from meter
+    PROC_DELAY_FACTOR = 0.1    # queueing or control latency at substations/fog servers
 
 
     # Simulation scenario
